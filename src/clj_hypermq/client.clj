10d1edc5862d8cc80fc040356921c1e9183a62dc
Christian Blunden <christian.blunden@gmail.com>
2014-04-29 00:09:24 +0100

    fetch messages by crawling prev archive links

--- src/clj_hypermq/client.clj
+++ src/clj_hypermq/client.clj
@@ -1,11 +1,25 @@
 (ns clj-hypermq.client
   (:require [clj-http.client :as http]))
 
+(defn- build-uri [host queue]
+  (str host "/q/" queue))
+
 (defn create-message
   [host queue title author content]
-  (let [uri (str host "/q/" queue)
-        msg {:title title :author author :content content}
-        result (http/post uri {:form-params msg :content-type :json})]
-    (case (result :status)
+  (let [msg {:title title :author author :content content}
+        response (http/post (build-uri host queue) {:form-params msg :content-type :json})]
+    (case (response :status)
       200 true
       false)))
+
+(defn lazy-messages
+  [uri]
+  (when uri
+      (let [response (http/get uri {:as :json})
+            messages (get-in response [:body :_embedded :message])
+            prev-page (get-in response [:body :_links :prev :href])]
+        (lazy-cat messages (lazy-messages prev-page)))))
+
+(defn fetch-messages
+  [host queue]
+  (lazy-messages (build-uri host queue)))
--- test/clj_hypermq/client_test.clj
+++ test/clj_hypermq/client_test.clj
@@ -20,3 +20,31 @@
       (create-message "http://server" "myqueue" "mytitle" "author" {:foo "bar"}) => false?
       (provided
        (http/post anything anything) => {:status 404}))
+
+(fact "fetch messages from queue uri"
+      (fetch-messages "http://server" "myqueue") => anything
+      (provided
+       (http/get "http://server/q/myqueue" {:as :json}) => anything))
+
+(fact "fetches a single message"
+      (fetch-messages "http://server" "myqueue") => [{:uuid "1" :title "mytitle" :author "author" :timestamp 54321}]
+      (provided
+       (http/get anything anything) => {:_embedded
+                                        {:message [{:uuid "1" :title "mytitle" :author "author" :timestamp 54321}]}}))
+
+(fact "will crawl prev links when present"
+      (fetch-messages "http://server" "myqueue") => anything
+      (provided
+       (http/get "http://server/q/myqueue" anything) => {:body {:_links {:prev {:href "http://server/q/myqueue/1"}}}}
+       (http/get "http://server/q/myqueue/1" anything) => {:body {:_links {:prev {:href "http://server/q/myqueue/0"}}}}
+       (http/get "http://server/q/myqueue/0" anything) => {:body {:_links {}}}))
+
+(fact "will fetch messages across pages"
+      (fetch-messages "http://server" "myqueue") => [{:uuid "3"} {:uuid "2"} {:uuid "1"}]
+      (provided
+       (http/get "http://server/q/myqueue" anything) => {:body {:_links {:prev {:href "http://server/q/myqueue/1"}}
+                                                                :_embedded {:message [{:uuid "3"}]}}}
+       (http/get "http://server/q/myqueue/1" anything) => {:body {:_links {:prev {:href "http://server/q/myqueue/0"}}
+                                                                  :_embedded {:message [{:uuid "2"}]}}}
+       (http/get "http://server/q/myqueue/0" anything) => {:body {:_links {}
+                                                                  :_embedded {:message [{:uuid "1"}]}}}))
