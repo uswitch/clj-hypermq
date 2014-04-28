8b871c001c259f932173751433ee5b8697c13049
Christian Blunden <christian.blunden@gmail.com>
2014-04-28 21:45:17 +0100

    initial commit

---
+++ test/clj_hypermq/client_test.clj
@@ -0,0 +1,7 @@
+(ns clj-hypermq.client-test
+  (:require [midje.sweet        :refer :all]
+            [clj-hypermq.client :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
