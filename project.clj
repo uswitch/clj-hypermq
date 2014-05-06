b58a804c8dd690434e6893fc08e3f5d9b43dfdd8
Christian Blunden <christian.blunden@gmail.com>
2014-05-06 13:43:59 +0100

    reset version to 0.0.1. deploy to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-hypermq "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-hypermq "0.0.1"
+  :description "Clojure client for hypermq"
+  :url "http://github.com/uswitch/clj-hypermq"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
